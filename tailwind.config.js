2d9fe9f4909184c7548d1fcbf31e4e6173c1a504
“Aiden <adnrdmnd@gmail.com>
2024-12-08 16:16:01 -0500

    navbar yay

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ['./**/*.html',],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
