fceb7206d9a15456cb82eb3489fe91530cd3def7
“Aiden <adnrdmnd@gmail.com>
2024-12-08 20:40:12 -0500

    photography working????

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
     content: ['./**/*.html',],
     theme: {
-      extend: {},
+      extend: {
+        colors : {
+            'black' : '#13150b',
+            'white' : '#DBDCD0',
+            'green' : '#3C4B35',
+            'light-blue' : '#a7cec2',
+            'dark-blue' : '#7dacb7'
+        }
+      },
     },
     plugins: [],
   }
