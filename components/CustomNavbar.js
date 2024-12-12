473bff49e8ddfa07f5a3dee024e56a79803409fb
“Aiden <adnrdmnd@gmail.com>
2024-12-11 21:29:58 -0500

    basic pages for everything

--- components/CustomNavbar.js
+++ components/CustomNavbar.js
@@ -14,14 +14,14 @@ class CustomNavbar extends HTMLElement {
         shadow.innerHTML += `
             <div class="flex justify-between items-center">
                 <a href="/"><img src="../images/logo_regular.png" alt="" width="64"></a>
-                <a href="#" class="hover:text-pink">ART</a>
-                <a href="#" class="hover:text-pink">CODE</a>
+                <a href="/art.html" class="hover:text-pink">ART</a>
+                <a href="/code.html" class="hover:text-pink">CODE</a>
                 <a href="/photography/photography-index.html" class="hover:text-pink">PHOTOGRAPHY</a>
-                <a href="#" class="hover:text-pink">RUNNING</a>
-                <a href="#" class="hover:text-pink">'TECH'</a>
-                <a href="#" class="hover:text-pink">CONTENT</a>
-                <a href="#" class="hover:text-pink">CURRENT WORK</a>
-                <a href="#" class="hover:text-pink">UPDATES</a>
+                <a href="/running.html" class="hover:text-pink">RUNNING</a>
+                <a href="/tech.html" class="hover:text-pink">'TECH'</a>
+                <a href="/content.html" class="hover:text-pink">CONTENT</a>
+                <a href="/current_work.html" class="hover:text-pink">CURRENT WORK</a>
+                <a href="/updates.html" class="hover:text-pink">UPDATES</a>
             </div>
         `;
     }
