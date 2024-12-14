b8c7500c85db878faebdb4a98c778baa867588c2
“Aiden <adnrdmnd@gmail.com>
2024-12-13 21:16:44 -0500

    refactor?

--- aidenredmond/src/views/RightView.vue
+++ aidenredmond/src/views/RightView.vue
@@ -1,8 +1,8 @@
 <template>
     <main class="flex justify-end flex-1 space-x-64">
         <div class="flex flex-col mt-36">
-            <h2>art/gd</h2>
             <h2>photography</h2>
+            <h2>art & graphic design</h2>
         </div>
         <div class="flex flex-col space-y-6">
             <h1 class="text-right font-bold text-5xl mt-12">right</h1>
