1945dd18ffc1a38e742ef121f39da2e74add1ff7
“Aiden <adnrdmnd@gmail.com>
2025-01-26 13:46:56 -0500

    remove redmond

--- aidenredmond/src/views/IndexView.vue
+++ aidenredmond/src/views/IndexView.vue
@@ -1,6 +1,6 @@
 <template>
     <main class="text-2xl sm:text-xl flex flex-col flex-1 items-center justify-center">
-        <h1 class="text-black text-center duration-500 transform opacity-0 transition-all translate-y-12 ease-in-out delay-500" data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'>hi, i'm aiden redmond.</h1>
+        <h1 class="text-black text-center duration-500 transform opacity-0 transition-all translate-y-12 ease-in-out delay-500" data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'>hi, i'm aiden.</h1>
         <h1 class="text-black text-center duration-500 transform opacity-0 transition-all translate-y-12 ease-in-out delay-[2000ms]" data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'>this website is split in half.</h1>
         <h1 class="text-black text-center duration-500 transform opacity-0 transition-all translate-y-12 ease-in-out delay-[4000ms]" data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'>check out the navbar ↑<br><br></h1>
         <div class="text-black text-sm duration-500 transform opacity-0 transition-all translate-y-12 ease-in-out delay-[6000ms]" data-replace='{ "translate-y-12": "translate-y-0", "opacity-0": "opacity-100" }'>
